561c345c00a10f499479c350643f4c38ee05076c
pmercier_sfdc <pmercier@salesforce.com>
2011-12-01 19:51:48 +0100

    oauth dance working, ready to save the auth_token into memcache

---
+++ app/controllers/Merchandise.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import play.mvc.Controller;
+
+public class Merchandise extends Controller {
+	
+    public static void index() {
+    	render();
+    }
+
+}
---
+++ app/controllers/Shop.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.*;
+
+public class Shop extends Controller {
+
+    public static void index() {
+    	// Load Merchandises
+        render();
+    }
+
+}
